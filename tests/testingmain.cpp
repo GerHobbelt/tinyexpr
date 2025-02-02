f336c810b1c88573337ada6031af23f1e21ffc76
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2025-02-02 10:46:50 -0500

    Fix tests build issue with latest Catch2

--- tests/testingmain.cpp
+++ tests/testingmain.cpp
@@ -1,14 +1,9 @@
 #include "../tinyexpr.h"
 #include <catch2/catch_session.hpp>
 #include <catch2/catch_test_macros.hpp>
-#include <cstdlib>
-#include <iostream>
-#include <string>
 
 int main(int argc, char** argv)
     {
-    std::cout << te_parser::info();
-
     Catch::Session session;
 
     session.applyCommandLine(argc, argv);
